e823bdb98b201b09d3c0bd9f067289ea45611dca
b7c <58299468+b7c@users.noreply.github.com>
2021-08-31 14:12:36 +1200

    add humanizer and habbo string converters

---
+++ b7.Xabbo/WPF/Converters/HabboStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+using Xabbo.Core;
+
+namespace b7.Xabbo.WPF.Converters
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class HabboStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string text)
+                throw new ArgumentException("Value is not a string.");
+
+            return H.RenderText(text);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ b7.Xabbo/WPF/Converters/HumanizerConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+using Humanizer;
+
+namespace b7.Xabbo.WPF.Converters
+{
+    [ValueConversion(typeof(object), typeof(string))]
+    public class HumanizerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString().Humanize() ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
