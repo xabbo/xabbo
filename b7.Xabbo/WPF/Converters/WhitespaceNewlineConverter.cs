88e38981f3141d6b5811a42b58a12bb423b3ce6e
b7c <58299468+b7c@users.noreply.github.com>
2021-09-03 10:14:59 +1200

    convert long whitespace to newlines in room description

---
+++ b7.Xabbo/WPF/Converters/WhitespaceNewlineConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+namespace b7.Xabbo.WPF.Converters
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class WhitespaceNewlineConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string s)
+            {
+                throw new ArgumentException("Input value must be a string.");
+            }
+
+            if (parameter is not int threshold)
+            {
+                threshold = 10;
+            }
+
+            return Regex.Replace(s, $@"[^\S\r\n]{{{threshold},}}", "\r\n");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
