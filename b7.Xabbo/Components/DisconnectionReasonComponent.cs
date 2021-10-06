245072229ba07887dc1ca30e8630cc25b5bb6bf6
b7c <58299468+b7c@users.noreply.github.com>
2021-10-07 02:02:27 +1300

    add disconnection reason component

---
+++ b7.Xabbo/Components/DisconnectionReasonComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Humanizer;
+
+using Xabbo.Core;
+using Xabbo.Interceptor;
+using Xabbo.Messages;
+
+namespace b7.Xabbo.Components
+{
+    public class DisconnectionReasonComponent : Component
+    {
+        public DisconnectionReasonComponent(IInterceptor interceptor)
+            : base(interceptor)
+        { }
+
+        [InterceptIn(nameof(Incoming.DisconnectionReason))]
+        protected void HandleDisconnectionReason(InterceptArgs e)
+        {
+            e.Block();
+
+            DisconnectReason reason = (DisconnectReason)e.Packet.ReadInt();
+
+            string reasonText = Enum.IsDefined(reason) ? reason.Humanize() : $"unknown ({(int)reason})";
+            string message = $"[xabbo] You were disconnected by the server.\n\nReason: {reasonText}";
+
+            Send(In.SystemBroadcast, message);
+        }
+    }
+}
