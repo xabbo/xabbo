a7306246b9822f778765078246e3ae5f1283bac6
b7c <58299468+b7c@users.noreply.github.com>
2021-12-23 05:46:04 +1300

    update flash window

--- b7.Xabbo/Components/FlashWindowComponent.cs
+++ b7.Xabbo/Components/FlashWindowComponent.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 
 using Microsoft.Extensions.Configuration;
 
@@ -11,53 +10,16 @@ using Xabbo.Core.Game;
 using Xabbo.Core.Events;
 using Xabbo.Core;
 
+using b7.Xabbo.Util;
+
 namespace b7.Xabbo.Components
 {
     public class FlashWindowComponent : Component
     {
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct FLASHWINFO
-        {
-            public uint cbSize;
-            public IntPtr hwnd;
-            public uint dwFlags;
-            public uint uCount;
-            public uint dwTimeout;
-        }
-
-        /// <summary>
-        /// Flash both the window caption and taskbar button.
-        /// This is equivalent to setting the FLASHW_CAPTION | FLASHW_TRAY flags.
-        /// </summary>
-        const uint FLASHW_ALL = 3;
-
-        /// <summary>
-        /// Flash continuously until the window comes to the foreground.
-        /// </summary>
-        const uint FLASHW_TIMERNOFG = 12;
-
-        private static bool FlashWindow(IntPtr windowHandle)
-        {
-            FLASHWINFO info = new();
-
-            info.cbSize = Convert.ToUInt32(Marshal.SizeOf(info));
-            info.hwnd = windowHandle;
-            info.dwFlags = FLASHW_ALL | FLASHW_TIMERNOFG;
-            info.uCount = uint.MaxValue;
-            info.dwTimeout = 0;
-
-            return FlashWindowEx(ref info);
-        }
-
         private readonly ProfileManager _profileManager;
         private readonly FriendManager _friendManager;
         private readonly RoomManager _roomManager;
 
-        private string? _currentUserName;
         private Process? _currentProcess;
 
         private bool _flashOnPrivateMessage;
@@ -121,40 +83,41 @@ namespace b7.Xabbo.Components
             _friendManager = friendManager;
             _roomManager = roomManager;
 
-            _profileManager.LoadedUserData += OnLoadedUserData;
             _friendManager.MessageReceived += OnReceivedPrivateMessage;
 
             _roomManager.EntitiesAdded += OnEntitiesAdded;
             _roomManager.EntityChat += OnEntityChat;
         }
 
-        private void FlashWindow()
+        protected override void OnDisconnected(object? sender, EventArgs e)
         {
-            if (_currentProcess is null) return;
+            base.OnDisconnected(sender, e);
 
-            FlashWindow(_currentProcess.MainWindowHandle);
+            _currentProcess = null;
         }
 
-        private void OnLoadedUserData(object? sender, EventArgs e)
+        private Process? FindProcess()
         {
-            UserData? userData = _profileManager.UserData;
-            
-            if (userData is null ||
-                userData.Name == _currentUserName)
-            {
-                return;
-            }
+            Process? process = null;
 
-            Process? process = Process.GetProcesses().FirstOrDefault(
-                p =>
+            UserData? userData = _profileManager.UserData;
+            if (userData is not null)
+            {
+                process = Process.GetProcesses().FirstOrDefault(p =>
                     p.MainWindowTitle.StartsWith("Habbo") &&
                     p.MainWindowTitle.EndsWith(userData.Name)
-            );
+                );
+            }
 
-            if (process is null) return;
+            return process;
+        }
 
-            _currentUserName = userData.Name;
-            _currentProcess = process;
+        private void FlashWindow()
+        {
+            _currentProcess ??= FindProcess();
+            if (_currentProcess is null) return;
+
+            WindowUtil.FlashWindow(_currentProcess.MainWindowHandle);
         }
 
         private void OnReceivedPrivateMessage(object? sender, FriendMessageEventArgs e)
--- b7.Xabbo/Util/WindowUtil.cs
+++ b7.Xabbo/Util/WindowUtil.cs
@@ -19,6 +19,40 @@ namespace b7.Xabbo.Util
         [DllImport("user32.dll")]
         static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
 
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct FLASHWINFO
+        {
+            public uint cbSize;
+            public IntPtr hwnd;
+            public uint dwFlags;
+            public uint uCount;
+            public uint dwTimeout;
+        }
+
+        const uint FLASHW_ALL = 3;
+
+        /// <summary>
+        /// Flash continuously until the window comes to the foreground.
+        /// </summary>
+        const uint FLASHW_TIMERNOFG = 12;
+
+        public static bool FlashWindow(IntPtr windowHandle)
+        {
+            FLASHWINFO info = new();
+
+            info.cbSize = Convert.ToUInt32(Marshal.SizeOf(info));
+            info.hwnd = windowHandle;
+            info.dwFlags = FLASHW_ALL | FLASHW_TIMERNOFG;
+            info.uCount = uint.MaxValue;
+            info.dwTimeout = 0;
+
+            return FlashWindowEx(ref info);
+        }
+
         public static void ActivateWindow(Window window)
         {
             var hwnd = new WindowInteropHelper(window).EnsureHandle();
