7a50b29ac145250f634843014cf9ae4b27b89626
b7c <58299468+b7c@users.noreply.github.com>
2024-02-17 12:13:31 +1300

    add DpiIndependentImage

---
+++ src/b7.Xabbo/WPF/Controls/DpiIndependentImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace b7.Xabbo.WPF.Controls;
+
+public class DpiIndependentImage : Image
+{
+    public DpiIndependentImage()
+    {
+        Stretch = System.Windows.Media.Stretch.Uniform;
+    }
+
+    protected override Size MeasureOverride(Size constraint)
+    {
+        if (Source is BitmapFrame frame)
+        {
+            return new Size(frame.PixelWidth, frame.PixelHeight);
+        }
+
+        return base.MeasureOverride(constraint);
+    }
+}
