1813d0340d9057359a04834c01c8540f030fa947
b7c <58299468+b7c@users.noreply.github.com>
2024-02-17 12:06:41 +1300

    add room access commands

--- src/b7.Xabbo/Commands/Modules/RoomCommands.cs
+++ src/b7.Xabbo/Commands/Modules/RoomCommands.cs
@@ -7,6 +7,7 @@ using Xabbo.Messages;
 using Xabbo.Interceptor;
 using Xabbo.Core.Game;
 using Xabbo.Core.Tasks;
+using Xabbo.Core;
 
 namespace b7.Xabbo.Commands;
 
@@ -53,31 +54,155 @@ public class RoomCommands : CommandModule
     }
 
     [Command("goto"), RequiredOut(nameof(Outgoing.FlatOpc))]
-    protected async Task GotoCommandHandler(CommandArgs args)
+    protected Task GotoCommandHandler(CommandArgs args)
     {
-        if (args.Count < 1 || !long.TryParse(args[0], out long roomId))
+        if (args.Count >= 1 && !long.TryParse(args[0], out long roomId))
+        {
+            string password = "";
+            if (args.Count > 1)
+                password = string.Join(" ", args.Skip(1));
+
+            Send(Out.FlatOpc, (LegacyLong)roomId, password, -1L);
+        }
+        else
         {
             ShowMessage("Usage: /goto <room id> [password]");
-            return;
         }
 
-        string password = "";
-        if (args.Count > 1)
-            password = string.Join(" ", args.Skip(1));
-
-        Send(Out.FlatOpc, (LegacyLong)roomId, password, -1L);
+        return Task.CompletedTask;
     }
 
     [Command("exit"), RequiredOut(nameof(Outgoing.FlatOpc))]
-    protected async Task ExitCommandHandler(CommandArgs args) => Send(Out.FlatOpc, 0, "", -1L);
+    protected Task ExitCommandHandler(CommandArgs args)
+    {
+        Send(Out.FlatOpc, 0, "", -1L);
+        return Task.CompletedTask;
+    }
 
     [Command("reload"), RequiredIn(nameof(Incoming.RoomForward))]
-    protected async Task ReloadCommandHandler(CommandArgs args)
+    protected Task ReloadCommandHandler(CommandArgs args)
     {
         if (roomManager.IsInRoom)
             Send(In.RoomForward, (LegacyLong)roomManager.CurrentRoomId);
+        return Task.CompletedTask;
     }
 
-    [Command("trigger"), RequiredOut(nameof(Outgoing.GetRoomEntryData))]
-    protected async Task TriggerEntryWiredCommandHandler(CommandArgs args) => Send(Out.GetRoomEntryData);
+    [Command("entry"), RequiredOut(nameof(Outgoing.GetRoomEntryData))]
+    protected Task TriggerEntryWiredCommandHandler(CommandArgs args)
+    {
+        Send(Out.GetRoomEntryData);
+        return Task.CompletedTask;
+    }
+
+    protected async Task UpdateRoomSettingsAsync(Action<RoomSettings> update)
+    {
+        if (!roomManager.IsInRoom)
+        {
+            ShowMessage("Room state is not being tracked, please re-enter the room.");
+            return;
+        }
+
+        if (!roomManager.IsOwner)
+        {
+            ShowMessage("You must be the owner of the room to change room settings.");
+            return;
+        }
+
+        var settings = await new GetRoomSettingsTask(Extension, roomManager.CurrentRoomId)
+            .ExecuteAsync(2000, CancellationToken.None);
+        update(settings);
+
+        var receiver = Extension.ReceiveAsync(
+            (In.RoomSettingsSaved, In.RoomSettingsSaveError),
+            timeout: 2000,
+            block: true
+        );
+        Send(Out.SaveRoomSettings, settings);
+
+        var packet = await receiver;
+        if (packet.Header == In.RoomSettingsSaveError)
+            throw new Exception("Failed to update room settings.");
+    }
+
+    [Command("lock")]
+    [RequiredIn(nameof(Incoming.RoomSettingsData))]
+    [RequiredOut(nameof(Outgoing.GetRoomSettings), nameof(Outgoing.SaveRoomSettings))]
+    protected async Task LockRoomAsync(CommandArgs args)
+    {
+        string? password = null;
+        if (args.Count > 0)
+        {
+            password = string.Join(' ', args);
+        }
+
+        await UpdateRoomSettingsAsync(s =>
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                s.Access = RoomAccess.Doorbell;
+            }
+            else
+            {
+                s.Password = password;
+                s.Access = RoomAccess.Password;
+            }
+        });
+
+    }
+
+    [Command("open")]
+    [RequiredIn(nameof(Incoming.RoomSettingsData))]
+    [RequiredOut(nameof(Outgoing.GetRoomSettings), nameof(Outgoing.SaveRoomSettings))]
+    protected async Task OpenRoomAsync(CommandArgs args)
+    {
+        await UpdateRoomSettingsAsync(s => s.Access = RoomAccess.Open);
+        ShowMessage("Room has been opened.");
+    }
+
+    [Command("access", "ra", Usage = "<open/hide/lock> [password]")]
+    [RequiredIn(nameof(Incoming.RoomSettingsData))]
+    [RequiredOut(nameof(Outgoing.GetRoomSettings), nameof(Outgoing.SaveRoomSettings))]
+    protected async Task SetRoomAccessAsync(CommandArgs args)
+    {
+        if (args.Count < 1)
+            throw new InvalidArgsException();
+
+        switch (args[0].ToUpperInvariant())
+        {
+            case "OPEN":
+                {
+                    await UpdateRoomSettingsAsync(s => s.Access = RoomAccess.Open);
+                    ShowMessage("Room has been opened.");
+                }
+                break;
+            case "HIDE":
+                {
+                    await UpdateRoomSettingsAsync(s => s.Access = RoomAccess.Invisible);
+                    ShowMessage("Room is now hidden.");
+                }
+                break;
+            case "LOCK":
+                {
+                    string? password = null;
+                    if (args.Count > 1)
+                        password = string.Join(' ', args.Skip(1));
+                    await UpdateRoomSettingsAsync(s =>
+                    {
+                        if (string.IsNullOrWhiteSpace(password))
+                        {
+                            s.Access = RoomAccess.Doorbell;
+                        }
+                        else
+                        {
+                            s.Password = password;
+                            s.Access = RoomAccess.Password;
+                        }
+                    });
+                    ShowMessage(string.IsNullOrWhiteSpace(password) ? "Room password has been set." : "Room has been locked.");
+                }
+                break;
+            default:
+                throw new InvalidArgsException();
+        }
+    }
 }
