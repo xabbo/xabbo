fad3c5330a0b7bde4d40c4eb1c70ff1021081851
rob <rob@b7c.io>
2024-09-22 05:28:06 +1200

    fix: items sometimes not displayed when switching to furni grid view

--- src/Xabbo.Avalonia/Views/Room/Furni/RoomFurniGridView.axaml.cs
+++ src/Xabbo.Avalonia/Views/Room/Furni/RoomFurniGridView.axaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Threading;
 
 namespace Xabbo.Views;
 
@@ -7,5 +9,13 @@ public partial class RoomFurniGridView : UserControl
     public RoomFurniGridView()
     {
         InitializeComponent();
+
+        FurniStacks.AttachedToVisualTree += OnFurniStacksAttached;
+    }
+
+    private void OnFurniStacksAttached(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        // Workaround for a weird bug where items have zero dimensions when switching to the furni grid view.
+        Dispatcher.UIThread.InvokeAsync(() => FurniStacks.InvalidateMeasure());
     }
 }
