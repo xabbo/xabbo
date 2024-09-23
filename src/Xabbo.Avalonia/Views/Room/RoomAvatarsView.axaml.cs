7f7d2bbe917868a05cf8602ab58e8fbd2d1ee189
rob <rob@b7c.io>
2024-09-24 07:15:12 +1200

    what

--- src/Xabbo.Avalonia/Views/Room/RoomAvatarsView.axaml.cs
+++ src/Xabbo.Avalonia/Views/Room/RoomAvatarsView.axaml.cs
@@ -1,121 +1,104 @@
-<Application
-  xmlns="https://github.com/avaloniaui"
-  xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-  x:Class="Xabbo.App"
-  xmlns:local="using:Xabbo"
-  xmlns:sty="using:FluentAvalonia.Styling"
-  xmlns:ui="using:FluentAvalonia.UI.Controls"
-  xmlns:ic="using:FluentIcons.Avalonia.Fluent"
-  xmlns:img="clr-namespace:AsyncImageLoader;assembly=AsyncImageLoader.Avalonia"
-  xmlns:services="using:Xabbo.Avalonia.Services"
-  xmlns:c="using:Xabbo.Converters"
-  RequestedThemeVariant="Dark"
->
-    <Application.DataTemplates>
-      <local:ViewLocator/>
-    </Application.DataTemplates>
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.LogicalTree;
 
-    <Application.Styles>
-      <sty:FluentAvaloniaTheme  />
-      <StyleInclude Source="avares://FluentAvalonia.ProgressRing/Styling/Controls/ProgressRing.axaml" />
-      <StyleInclude Source="avares://AsyncImageLoader.Avalonia/AdvancedImage.axaml" />
-      <Style Selector="img|AdvancedImage">
-        <Setter Property="Template">
-          <ControlTemplate>
-            <Grid/>
-          </ControlTemplate>
-        </Setter>
-        <Style Selector="^.spinner">
-          <Setter Property="Template">
-            <ControlTemplate>
-              <Grid>
-                <ui:ProgressRing
-                  HorizontalAlignment="Center"
-                  VerticalAlignment="Center"
-                  Width="24" Height="24"
-                  IsVisible="{TemplateBinding IsLoading}"
-                />
-              </Grid>
-            </ControlTemplate>
-          </Setter>
-        </Style>
-      </Style>
+using Xabbo.ViewModels;
 
-      <Style Selector=":is(Control).origins :is(Control).modern">
-        <Setter Property="IsVisible" Value="False" />
-      </Style>
+namespace Xabbo.Views;
 
-      <Style Selector="SelectableTextBlock">
-        <Setter Property="SelectionBrush" Value="Purple" />
-      </Style>
+public partial class RoomAvatarsView : UserControl
+{
+    private DataGridCollectionView? _collectionView;
 
-      <Style Selector="DataGrid DataGridCell:current /template/ Grid#FocusVisual">
-        <Setter Property="IsVisible" Value="False" />
-      </Style>
+    private RoomAvatarsViewModel? _viewModel;
 
-      <Style Selector="ic|SymbolIcon.filled">
-        <Setter Property="IconVariant" Value="Filled" />
-      </Style>
+    public RoomAvatarsView()
+    {
+        InitializeComponent();
 
-      <Style Selector="RadioButton">
-        <Setter Property="Margin" Value="0,0,5,0" />
-      </Style>
+        DataContextChanged += OnDataContextChanged;
+        AvatarDataGrid.Sorting += OnSorting;
+        DetachedFromLogicalTree += OnDetached;
+        AttachedToVisualTree += OnAttachedToVisualTree;
+        DetachedFromVisualTree += OnDetachedFromVisualTree;
+    }
 
-      <!-- Slimmer tab items -->
-      <Style Selector="TabItem">
-        <Setter Property="MinHeight" Value="40" />
-        <Setter Property="FontSize" Value="14" />
-      </Style>
+    private void OnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        AvatarDataGrid.ItemsSource = _collectionView;
+    }
 
-      <!-- DataGrid -->
-      <Style Selector="TextBlock#CellTextBlock">
-        <Setter Property="TextTrimming" Value="CharacterEllipsis" />
-      </Style>
-      <Style Selector="DataGridColumnHeader:sortascending /template/ ui|FontIcon#SortIcon">
-        <Setter Property="FontSize" Value="10" />
-        <Setter Property="Margin" Value="0" />
-      </Style>
-      <Style Selector="DataGridColumnHeader:sortdescending /template/ ui|FontIcon#SortIcon">
-        <Setter Property="FontSize" Value="10" />
-        <Setter Property="Margin" Value="0" />
-      </Style>
-    </Application.Styles>
+    private void OnDetachedFromVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        AvatarDataGrid.ItemsSource = null;
+    }
 
-  <Application.Resources>
-    <ResourceDictionary>
-      <ResourceDictionary.MergedDictionaries>
-        <ResourceInclude Source="Controls/Loading.axaml" />
-      </ResourceDictionary.MergedDictionaries>
+    private void OnDetached(object? sender, LogicalTreeAttachmentEventArgs e)
+    {
+        if (_viewModel is not null)
+            _viewModel.RefreshList -= OnRefreshList;
+    }
 
-      <!-- Session -->
-      <!-- Fix: make checkbox padding consistent across different platforms -->
-      <Thickness x:Key="CheckBoxPadding">8,5,8,5</Thickness>
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        if (_viewModel is not null)
+            return;
 
-      <c:HabboStringConverter x:Key="HabboStringConverter"/>
-      <c:MultiValueConverter x:Key="MultiValueConverter"/>
-      <x:Boolean x:Key="IsConnecting">True</x:Boolean>
-      <x:Boolean x:Key="IsConnected">False</x:Boolean>
-      <x:Boolean x:Key="IsOrigins">False</x:Boolean>
-      <x:Boolean x:Key="IsModern">False</x:Boolean>
-      <x:String x:Key="AppStatus">Waiting for connection...</x:String>
+        _viewModel = DataContext as RoomAvatarsViewModel;
+        if (_viewModel is null)
+            return;
 
-      <Color x:Key="RadioButtonCheckGlyphFill">#5000AA</Color>
-      <Color x:Key="RadioButtonCheckGlyphFillPointerOver">#5000AA</Color>
-      <Color x:Key="RadioButtonCheckGlyphFillPressed">#5000AA</Color>
-      <Color x:Key="RadioButtonCheckGlyphFillDisabled">#333333</Color>
+        _collectionView = new DataGridCollectionView(_viewModel.Avatars, false, false);
+        _collectionView.GroupDescriptions.Add(new DataGridPathGroupDescription(nameof(AvatarViewModel.Group)));
+        _collectionView.SortDescriptions.Add(new DataGridComparerSortDescription(
+            AvatarViewModelGroupComparer.Default,
+            ListSortDirection.Ascending
+        ));
+        _viewModel.RefreshList += OnRefreshList;
 
-      <!-- Fix: make checkbox padding consistent across different platforms -->
-      <Thickness x:Key="CheckBoxPadding">8,5,8,5</Thickness>
+        AvatarDataGrid.ItemsSource = _collectionView;
+    }
 
-      <c:HabboStringConverter x:Key="HabboStringConverter"/>
-      <c:MultiValueConverter x:Key="MultiValueConverter"/>
-      <c:BoolToOpacityConverter x:Key="BoolToOpacityConverter"/>
-      <c:HumanizerConverter x:Key="HumanizerConverter"/>
-      <c:WhitespaceNewlineConverter x:Key="WhitespaceNewlineConverter" />
-      <c:GtConverter x:Key="GtConverter" />
-      <c:EqualityConverter x:Key="EqConverter"/>
-      <c:InequalityConverter x:Key="NeqConverter"/>
+    private void OnSorting(object? sender, DataGridColumnEventArgs e)
+    {
+        if (_collectionView is null) return;
 
-    </ResourceDictionary>
-  </Application.Resources>
-</Application>
+        var sortDescription = _collectionView
+            .SortDescriptions
+            .FirstOrDefault(x => x.PropertyPath == e.Column.SortMemberPath);
+
+        _collectionView.SortDescriptions.Clear();
+        _collectionView.SortDescriptions.Add(
+            new DataGridComparerSortDescription(AvatarViewModelGroupComparer.Default, ListSortDirection.Ascending));
+
+        if (sortDescription is not null)
+        {
+            _collectionView.SortDescriptions.Add(sortDescription.SwitchSortDirection());
+        }
+        else
+        {
+            _collectionView.SortDescriptions.Add(DataGridSortDescription.FromPath(
+                e.Column.SortMemberPath,
+                ListSortDirection.Ascending,
+                e.Column.SortMemberPath switch
+                {
+                    "Index" => Comparer<int>.Default,
+                    "Id" => Comparer<long>.Default,
+                    "Name" => StringComparer.OrdinalIgnoreCase,
+                    _ => throw new Exception($"Unknown sort member: '{e.Column.SortMemberPath}'.")
+                }
+            ));
+        }
+        e.Handled = true;
+    }
+
+    private void OnRefreshList()
+    {
+        _collectionView?.Refresh();
+    }
+}
