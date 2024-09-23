124c0b4fa759e6623210b73b871d4a76c546b3b6
rob <rob@b7c.io>
2024-09-24 07:16:12 +1200

    wait for user data in app session manager

--- src/Xabbo.Avalonia/Services/AppSessionManager.cs
+++ src/Xabbo.Avalonia/Services/AppSessionManager.cs
@@ -1,7 +1,10 @@
 using System.ComponentModel;
+using System.Threading;
 using System.Threading.Tasks;
 using Avalonia;
 using Microsoft.Extensions.Hosting;
+using Xabbo.Core.Game;
+using Xabbo.Core.GameData;
 
 using Xabbo.Extension;
 using Xabbo.Services.Abstractions;
@@ -14,21 +17,32 @@ public class AppSessionManager
     private readonly IHostApplicationLifetime _lifetime;
     private readonly IUiContext _uiContext;
     private readonly IExtension _extension;
+    private readonly IGameDataManager _gameDataManager;
+    private readonly ProfileManager _profileManager;
 
     public AppSessionManager(Application application,
         IHostApplicationLifetime lifetime,
         IUiContext uiContext,
-        IExtension extension)
+        IExtension extension,
+        IGameDataManager gameDataManager,
+        ProfileManager profileManager)
     {
         _application = application;
         _lifetime = lifetime;
         _uiContext = uiContext;
         _extension = extension;
+        _gameDataManager = gameDataManager;
+        _profileManager = profileManager;
 
         _extension.Connected += OnConnected;
         _extension.Disconnected += OnDisconnected;
 
         lifetime.ApplicationStopping.Register(OnStopping);
+    }
+
+    private void SetStatus(string status)
+    {
+        _uiContext.Invoke(() => _application.Resources["AppStatus"] = status);
     }
 
     private void OnConnected(GameConnectedArgs e)
@@ -40,14 +54,37 @@ public class AppSessionManager
             _application.Resources["IsOrigins"] = e.Session.IsShockwave;
             _application.Resources["IsModern"] = !e.Session.IsShockwave;
         });
+
+        CancellationToken ct = _extension.DisconnectToken;
+        Task.Run(() => InitializeAsync(e.Session, ct));
+    }
+
+    private async Task InitializeAsync(Session session, CancellationToken ct)
+    {
+        try
+        {
+            SetStatus($"Loading game data for {session.Hotel.Name} hotel...");
+            await _gameDataManager.WaitForLoadAsync(ct);
+
+            SetStatus($"Waiting for user data...");
+            await _profileManager.GetUserDataAsync();
+
+            _uiContext.Invoke(() =>
+            {
+                _application.Resources["IsReady"] = true;
+            });
+        }
+        catch { }
     }
 
     private void OnDisconnected()
     {
         _uiContext.Invoke(() =>
         {
+            _application.Resources["AppStatus"] = "Waiting for connection...";
             _application.Resources["IsConnecting"] = true;
             _application.Resources["IsConnected"] = false;
+            _application.Resources["IsReady"] = false;
         });
     }
 
