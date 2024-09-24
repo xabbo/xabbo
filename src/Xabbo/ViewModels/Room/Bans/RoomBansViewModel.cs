bf45d0122b7d9662b68a536d448a41d534bff958
rob <rob@b7c.io>
2024-09-24 20:06:15 +1200

    update room bans view

--- src/Xabbo/ViewModels/Room/Bans/RoomBansViewModel.cs
+++ src/Xabbo/ViewModels/Room/Bans/RoomBansViewModel.cs
@@ -1,47 +1,69 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.ObjectModel;
 using System.Reactive;
-using System.Threading.Tasks;
 using DynamicData;
 using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
+using HanumanInstitute.MvvmDialogs;
+using HanumanInstitute.MvvmDialogs.Avalonia.Fluent;
 
 using Xabbo.Extension;
-using Xabbo.Messages.Flash;
 using Xabbo.Core.Game;
+using Xabbo.Core.Messages.Outgoing.Modern;
 using Xabbo.Services.Abstractions;
+using Microsoft.Extensions.Logging;
 
 namespace Xabbo.ViewModels;
 
 public class RoomBansViewModel : ViewModelBase
 {
+    private readonly ILogger Log;
     private readonly IUiContext _uiCtx;
+    private readonly IDialogService _dialogService;
+    private readonly IOperationManager _operationManager;
     private readonly IExtension _ext;
     private readonly RoomManager _roomManager;
 
-    private readonly SourceCache<RoomBanViewModel, long> _banCache = new(x => x.Id);
+    private readonly SourceCache<RoomBanViewModel, Id> _banCache = new(x => x.Id);
+    private readonly ReadOnlyObservableCollection<RoomBanViewModel> _bans;
+    public ReadOnlyObservableCollection<RoomBanViewModel> Bans => _bans;
 
     [Reactive] public string FilterText { get; set; } = "";
     [Reactive] public bool CanLoad { get; set; } = true;
     [Reactive] public bool IsLoading { get; set; }
 
-    public ReactiveCommand<Unit, Unit> UnbanCommand { get; }
-    public ReactiveCommand<Unit, Unit> LoadBansCommand { get; }
+    public ReactiveCommand<Unit, Unit> LoadBansCmd { get; }
+    public ReactiveCommand<IList, Unit> UnbanSelectedUsersCmd { get; }
 
-    public RoomBansViewModel(IUiContext uiContext, IExtension extension, RoomManager roomManager)
+    public RoomBansViewModel(
+        ILoggerFactory loggerFactory,
+        IUiContext uiContext,
+        IDialogService dialogService,
+        IOperationManager operationManager,
+        IExtension extension,
+        RoomManager roomManager)
     {
+        Log = loggerFactory.CreateLogger<RoomBansViewModel>();
         _uiCtx = uiContext;
+        _dialogService = dialogService;
+        _operationManager = operationManager;
         _ext = extension;
         _roomManager = roomManager;
 
+        _banCache
+            .Connect()
+            .Filter(FilterBans)
+            .Bind(out _bans)
+            .Subscribe();
+
         _roomManager.Left += OnLeftRoom;
 
-        UnbanCommand = ReactiveCommand.CreateFromTask(UnbanAsync);
-        LoadBansCommand = ReactiveCommand.CreateFromTask(LoadBansAsync);
+        LoadBansCmd = ReactiveCommand.CreateFromTask(LoadBansAsync);
+        UnbanSelectedUsersCmd = ReactiveCommand.CreateFromTask<IList>(UnbanSelectedUsersAsync);
     }
 
-    private Task UnbanAsync()
+    private bool FilterBans(RoomBanViewModel ban)
     {
-        return Task.CompletedTask;
+        return true;
     }
 
     private void OnLeftRoom()
@@ -56,37 +78,81 @@ public class RoomBansViewModel : ViewModelBase
 
         try
         {
+            Log.LogDebug("Loading bans...");
+
             IsLoading = true;
 
-            var receiver = _ext.ReceiveAsync(In.BannedUsersFromRoom, timeout: 3000, block: true);
-            _ext.Send(Out.GetBannedUsersFromRoom, currentRoomId);
-            var packet = await receiver;
-            long roomId = packet.Read<Id>();
-            if (roomId != currentRoomId)
+            var msg = await _ext.RequestAsync(new GetBannedUsersMsg(currentRoomId), timeout: 3000);
+            _banCache.Clear();
+
+            if (msg.RoomId != currentRoomId)
             {
                 throw new Exception("Room ID mismatch");
             }
 
-            int n = packet.Read<Length>();
-            var viewModels = new RoomBanViewModel[n];
-            for (int i = 0; i < n; i++)
-            {
-                var (id, name) = packet.Read<Id, string>();
-                viewModels[i] = new RoomBanViewModel(id, name);
-            }
+            Log.LogInformation("Loaded {Count} bans.", msg.Users.Count);
+
+            var viewModels = msg.Users.Select(x => new RoomBanViewModel(x.Id, x.Name)).ToArray();
 
             _uiCtx.Invoke(() =>
             {
                 foreach (var vm in viewModels)
-                {
                     _banCache.AddOrUpdate(vm);
-                }
             });
         }
-        catch { }
+        catch
+        {
+            _banCache.Clear();
+        }
         finally
         {
             IsLoading = false;
+        }
+    }
+
+    private Task UnbanSelectedUsersAsync(IList selection) => UnbanAsync(selection.OfType<RoomBanViewModel>());
+
+    private async Task UnbanAsync(IEnumerable<RoomBanViewModel> users)
+    {
+        if (!_roomManager.IsInRoom)
+            return;
+
+        Id roomId = _roomManager.CurrentRoomId;
+        if (roomId <= 0) return;
+
+        try
+        {
+            var array = users.ToArray();
+            if (array.Length == 0) return;
+
+            Log.LogDebug("Unbanning {Count} users.", array.Length);
+
+            await _operationManager.RunAsync("unban users", async cancellationToken => {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (i > 0)
+                        await Task.Delay(500, cancellationToken);
+                    Log.LogTrace("Unbanning user '{Name}'.", array[i].Name);
+                    await _ext.RequestAsync(new UnbanUserMsg(roomId, array[i].Id), cancellationToken: cancellationToken);
+                    _banCache.RemoveKey(array[i].Id);
+                }
+            });
+        }
+        catch (Exception ex)
+        {
+            await _dialogService.ShowContentDialogAsync(
+                _dialogService.CreateViewModel<MainViewModel>(),
+                new ContentDialogSettings
+                {
+                    Title = "Error",
+                    Content = $"Failed to retrieve ban list: {ex.Message}",
+                    PrimaryButtonText = "OK"
+                }
+            );
+        }
+        finally
+        {
+
         }
     }
 }
