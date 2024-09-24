c2d820a09acaf81d59af9f9fd0a29b9a5845e2af
rob <rob@b7c.io>
2024-09-25 04:35:23 +1200

    implement autoscroll in chat page

--- src/Xabbo.Avalonia/Views/Pages/ChatPage.axaml.cs
+++ src/Xabbo.Avalonia/Views/Pages/ChatPage.axaml.cs
@@ -1,24 +1,46 @@
 ﻿using System;
-
+using System.Reactive.Linq;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+using Xabbo.ViewModels;
 
 namespace Xabbo.Views;
 
 public partial class ChatPage : UserControl
 {
+    private IDisposable? _watchText;
+
+    [Reactive] public bool IsAutoScrollEnabled { get; set; }
+
     public ChatPage()
     {
         InitializeComponent();
 
-        ChatScrollViewer.ScrollChanged += ChatScrollViewer_ScrollChanged;
+        DataContextChanged += OnDataContextChanged;
+        Unloaded += OnUnloaded;
     }
 
-    private void ChatScrollViewer_ScrollChanged(object? sender, ScrollChangedEventArgs e)
+    private void OnDataContextChanged(object? sender, EventArgs e)
     {
+        if (_watchText is not null) return;
+
+        if (DataContext is ChatPageViewModel chatPage)
+        {
+            _watchText = chatPage
+                .WhenAnyValue(x => x.LogText)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(x => {
+                    if (IsAutoScrollEnabled)
+                        ChatScrollViewer.ScrollToEnd();
+                });
+        }
     }
 
-    protected override void OnDataContextChanged(EventArgs e)
+    private void OnUnloaded(object? sender, RoutedEventArgs e)
     {
-        base.OnDataContextChanged(e);
+        _watchText?.Dispose();
     }
 }
