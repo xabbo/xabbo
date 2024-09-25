2a0a741a471f02a42a5a8f17ec0f77807901e5e7
rob <rob@b7c.io>
2024-09-25 17:35:41 +1200

    adjust app session management

--- src/Xabbo.Avalonia/Services/AppSessionManager.cs
+++ src/Xabbo.Avalonia/Services/AppSessionManager.cs
@@ -1,12 +1,18 @@
+using System;
 using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
 using Avalonia;
+using Humanizer;
 using Microsoft.Extensions.Hosting;
+using Xabbo.Core;
 using Xabbo.Core.Game;
 using Xabbo.Core.GameData;
+using Xabbo.Core.Messages.Incoming.Modern;
 
 using Xabbo.Extension;
+using Xabbo.Messages;
+using Xabbo.Messages.Flash;
 using Xabbo.Services.Abstractions;
 
 namespace Xabbo.Avalonia.Services;
@@ -19,6 +25,8 @@ public class AppSessionManager
     private readonly IExtension _extension;
     private readonly IGameDataManager _gameDataManager;
     private readonly ProfileManager _profileManager;
+
+    private DisconnectReason _currentDisconnectReason = DisconnectReason.Unknown;
 
     public AppSessionManager(Application application,
         IHostApplicationLifetime lifetime,
@@ -40,6 +48,7 @@ public class AppSessionManager
         lifetime.ApplicationStopping.Register(OnStopping);
     }
 
+
     private void SetStatus(string status)
     {
         _uiContext.Invoke(() => _application.Resources["AppStatus"] = status);
@@ -47,6 +56,8 @@ public class AppSessionManager
 
     private void OnConnected(GameConnectedArgs e)
     {
+        _extension.Intercept<DisconnectReasonMsg>(HandleDisconnectReason);
+
         _uiContext.Invoke(() =>
         {
             _application.Resources["IsConnecting"] = false;
@@ -77,11 +88,21 @@ public class AppSessionManager
         catch { }
     }
 
+    void HandleDisconnectReason(DisconnectReasonMsg msg)
+    {
+        _currentDisconnectReason = msg.Reason;
+    }
+
     private void OnDisconnected()
     {
         _uiContext.Invoke(() =>
         {
-            _application.Resources["AppStatus"] = "Waiting for connection...";
+            _application.Resources["AppStatus"] = _currentDisconnectReason switch
+            {
+                DisconnectReason.Unknown or
+                DisconnectReason.Disconnected => "Disconnected!\nWaiting for another connection...",
+                _ => $"Disconnected! {_currentDisconnectReason.Humanize()}.\nWaiting for another connection..."
+            };
             _application.Resources["IsConnecting"] = true;
             _application.Resources["IsConnected"] = false;
             _application.Resources["IsReady"] = false;
@@ -98,6 +119,6 @@ public class AppSessionManager
         });
 
         // Delay shutdown for visual effect.
-        Task.Delay(6000).GetAwaiter().GetResult();
+        Task.Delay(5000).GetAwaiter().GetResult();
     }
 }
