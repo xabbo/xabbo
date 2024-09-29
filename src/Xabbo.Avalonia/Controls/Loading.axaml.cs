a765e26453ecba50cf085b45a902a4ff73e08115
rob <rob@b7c.io>
2024-09-29 21:20:29 +1300

    adjust Loading component

--- src/Xabbo.Avalonia/Controls/Loading.axaml.cs
+++ src/Xabbo.Avalonia/Controls/Loading.axaml.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Windows.Input;
 using Avalonia;
+using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Primitives;
 
 namespace Xabbo.Avalonia.Controls;
 
@@ -11,6 +16,42 @@ public class Loading : ContentControl
     public static readonly StyledProperty<object?> LoadingContentProperty =
         AvaloniaProperty.Register<Loading, object?>(nameof(LoadingContent));
 
+    public static readonly StyledProperty<ICommand?> CancelProperty =
+        AvaloniaProperty.Register<Loading, ICommand?>(nameof(CancelProperty));
+
+    public static readonly StyledProperty<TimeSpan> FadeDurationProperty =
+        AvaloniaProperty.Register<Loading, TimeSpan>(nameof(FadeDurationProperty), defaultValue: TimeSpan.FromSeconds(0.5));
+
+    public static readonly StyledProperty<TimeSpan?> FadeInDurationProperty =
+        AvaloniaProperty.Register<Loading, TimeSpan?>(nameof(FadeInDurationProperty), coerce: CoerceFade);
+
+    public static readonly StyledProperty<TimeSpan?> FadeOutDurationProperty =
+        AvaloniaProperty.Register<Loading, TimeSpan?>(nameof(FadeOutDurationProperty), coerce: CoerceFade);
+
+    public static readonly StyledProperty<double> ContentFadeOpacityProperty =
+        AvaloniaProperty.Register<Loading, double>(nameof(ContentFadeOpacityProperty), defaultValue: 0);
+
+    private static TimeSpan? CoerceFade(AvaloniaObject @object, TimeSpan? nullable)
+        => nullable is { } value ? value : @object.GetValue(FadeDurationProperty);
+
+    public TimeSpan FadeDuration
+    {
+        get => GetValue(FadeDurationProperty);
+        set => SetValue(FadeDurationProperty, value);
+    }
+
+    public TimeSpan? FadeInDuration
+    {
+        get => GetValue(FadeInDurationProperty) ?? GetValue(FadeDurationProperty);
+        set => SetValue(FadeInDurationProperty, value);
+    }
+
+    public TimeSpan? FadeOutDuration
+    {
+        get => GetValue(FadeOutDurationProperty) ?? GetValue(FadeDurationProperty);
+        set => SetValue(FadeOutDurationProperty, value);
+    }
+
     public bool IsLoading
     {
         get => GetValue(IsLoadingProperty);
@@ -21,5 +62,17 @@ public class Loading : ContentControl
     {
         get => GetValue(LoadingContentProperty);
         set => SetValue(LoadingContentProperty, value);
+    }
+
+    public double ContentFadeOpacity
+    {
+        get => GetValue(ContentFadeOpacityProperty);
+        set => SetValue(ContentFadeOpacityProperty, value);
+    }
+
+    public ICommand? Cancel
+    {
+        get => GetValue(CancelProperty);
+        set => SetValue(CancelProperty, value);
     }
 }
