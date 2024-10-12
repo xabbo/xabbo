17db2a7457d17d7d71938e2c1ead6d52085f4875
rob <rob@b7c.io>
2024-10-12 17:51:18 +1300

    update find friend command

--- src/Xabbo/Command/Modules/FindFriendCommand.cs
+++ src/Xabbo/Command/Modules/FindFriendCommand.cs
@@ -3,17 +3,17 @@ using Xabbo.Messages.Flash;
 using Xabbo.Core;
 using Xabbo.Core.Game;
 using Xabbo.Core.Messages.Outgoing;
+using System.Reactive.Linq;
 
 namespace Xabbo.Command.Modules;
 
-[CommandModule(SupportedClients = ~ClientType.Shockwave)]
+[CommandModule(SupportedClients = ClientType.Modern)]
 public sealed class FindFriendCommand(FriendManager friendManager) : CommandModule
 {
+    private readonly SemaphoreSlim _throttle = new(1, 1);
     private readonly FriendManager _friendManager = friendManager;
 
-    [Command("find", Usage = "<friendName>")]
-    // [RequiredOut(nameof(Out.FollowFriend))]
-    // [RequiredIn(nameof(In.RoomForward), nameof(In.FollowFriendFailed))]
+    [Command("find", Usage = "<friend name>")]
     public async Task OnFind(CommandArgs args)
     {
         if (args.Length < 1)
@@ -26,7 +26,7 @@ public sealed class FindFriendCommand(FriendManager friendManager) : CommandModu
         }
 
         var friend = _friendManager.Friends
-            .Where(x => x.Name.ToLower().Contains(args[0]))
+            .Where(x => x.Name.Contains(args[0], StringComparison.CurrentCultureIgnoreCase))
             .OrderBy(x => Math.Abs(x.Name.Length - args[0].Length))
             .FirstOrDefault();
 
@@ -36,37 +36,47 @@ public sealed class FindFriendCommand(FriendManager friendManager) : CommandModu
             return;
         }
 
-        Task<IPacket> receiveAsync = Ext.ReceiveAsync([In.RoomForward, In.FollowFriendFailed], 2000, true);
-        Ext.Send(Out.FollowFriend, friend.Id);
-        var packet = await receiveAsync;
+        await _throttle.WaitAsync();
+        var throttleInterval = Task.Delay(5000);
+        try
+        {
+            Task<IPacket> receiver = Ext.ReceiveAsync([In.RoomForward, In.FollowFriendFailed], 2000, true);
+            Ext.Send(Out.FollowFriend, friend.Id);
+            var packet = await receiver;
 
-        if (Ext.Messages.Is(packet.Header, In.RoomForward))
-        {
-            int roomId = packet.Read<int>();
-            var roomData = await Ext.RequestAsync(new GetRoomDataMsg(roomId));
-            ShowMessage($"{friend.Name} is in room '{roomData.Name}' by {roomData.OwnerName} (id:{roomData.Id}){(roomData.IsInvisible ? "*" : "")}");
-        }
-        else
-        {
-            var error = (FollowFriendError)packet.Read<int>();
-            switch (error)
+            if (Ext.Messages.Is(packet.Header, In.RoomForward))
             {
-                case FollowFriendError.NotFriend:
-                    ShowMessage($"{friend.Name} is not in your friend list");
-                    break;
-                case FollowFriendError.Offline:
-                    ShowMessage($"{friend.Name} is offline");
-                    break;
-                case FollowFriendError.NotInRoom:
-                    ShowMessage($"{friend.Name} is not in a room");
-                    break;
-                case FollowFriendError.CannotFollow:
-                    ShowMessage($"{friend.Name} has follow disabled");
-                    break;
-                default:
-                    ShowMessage($"Unknown error {error}");
-                    break;
+                int roomId = packet.Read<int>();
+                var roomData = await Ext.RequestAsync(new GetRoomDataMsg(roomId));
+                ShowMessage($"{friend.Name} is in room '{roomData.Name}' by {roomData.OwnerName} (id:{roomData.Id}){(roomData.IsInvisible ? "*" : "")}");
             }
+            else
+            {
+                var error = (FollowFriendError)packet.Read<int>();
+                switch (error)
+                {
+                    case FollowFriendError.NotFriend:
+                        ShowMessage($"{friend.Name} is not in your friend list");
+                        break;
+                    case FollowFriendError.Offline:
+                        ShowMessage($"{friend.Name} is offline");
+                        break;
+                    case FollowFriendError.NotInRoom:
+                        ShowMessage($"{friend.Name} is not in a room");
+                        break;
+                    case FollowFriendError.CannotFollow:
+                        ShowMessage($"{friend.Name} has follow disabled");
+                        break;
+                    default:
+                        ShowMessage($"Unknown error {error}");
+                        break;
+                }
+            }
+        }
+        finally
+        {
+            await throttleInterval;
+            _throttle.Release();
         }
     }
 }
