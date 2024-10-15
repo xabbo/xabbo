c80971591978bbb5f4b8bec310c63f5e52fb652f
rob <rob@b7c.io>
2024-10-15 14:37:28 +1300

    show assembly versions on info page

--- src/Xabbo/ViewModels/Info/InfoPageViewModel.cs
+++ src/Xabbo/ViewModels/Info/InfoPageViewModel.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using FluentIcons.Common;
 using FluentIcons.Avalonia.Fluent;
 using IconSource = FluentAvalonia.UI.Controls.IconSource;
@@ -8,4 +9,38 @@ public sealed class InfoPageViewModel : PageViewModel
 {
     public override string Header => "Info";
     public override IconSource? Icon { get; } = new SymbolIconSource { Symbol = Symbol.Info };
+
+    public string Version { get; }
+    public string XabboCommonVersion { get; }
+    public string XabboGEarthVersion { get; }
+    public string XabboMessagesVersion { get; }
+    public string XabboCoreVersion { get; }
+
+    public InfoPageViewModel()
+    {
+        Version = GetVersionString(Assembly.GetEntryAssembly());
+        XabboCommonVersion = GetVersionString(typeof(Xabbo.Client).Assembly);
+        XabboGEarthVersion = GetVersionString(typeof(Xabbo.GEarth.GEarthExtension).Assembly);
+        XabboMessagesVersion = GetVersionString(typeof(Xabbo.Messages.Flash.Out).Assembly);
+        XabboCoreVersion = GetVersionString(typeof(Xabbo.Core.H).Assembly);
+    }
+
+    static string GetVersionString(Assembly? assembly)
+    {
+        string? version = assembly?
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+            ?? assembly?.GetName().Version?.ToString(3);
+
+        if (version is null)
+            return "unknown version";
+
+        if (!version.StartsWith('v'))
+            version = "v" + version;
+
+        int index = version.IndexOf('+');
+        if (index > 0)
+            version = version[..index];
+
+        return version;
+    }
 }
