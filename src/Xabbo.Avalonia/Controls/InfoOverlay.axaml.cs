e345fe92154e6303527d17dd04ee285a632853ab
rob <rob@b7c.io>
2024-10-20 16:39:35 +1300

    allow additional content in InfoOverlay

--- src/Xabbo.Avalonia/Controls/InfoOverlay.axaml.cs
+++ src/Xabbo.Avalonia/Controls/InfoOverlay.axaml.cs
@@ -1,9 +1,34 @@
+using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
 
 namespace Xabbo.Avalonia.Controls;
 
 public class InfoOverlay : ContentControl
 {
+    public static readonly StyledProperty<string?> TextProperty
+        = AvaloniaProperty.Register<InfoOverlay, string?>(nameof(Text));
 
+    public static readonly StyledProperty<bool> ShowMessageProperty
+        = AvaloniaProperty.Register<InfoOverlay, bool>(nameof(ShowMessage), defaultValue: true);
+
+    public static readonly StyledProperty<bool> ShowContentProperty
+        = AvaloniaProperty.Register<InfoOverlay, bool>(nameof(ShowContent), defaultValue: true);
+
+    public string? Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public bool ShowMessage
+    {
+        get => GetValue(ShowMessageProperty);
+        set => SetValue(ShowMessageProperty, value);
+    }
+
+    public bool ShowContent
+    {
+        get => GetValue(ShowContentProperty);
+        set => SetValue(ShowContentProperty, value);
+    }
 }
