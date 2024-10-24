2d22b49a268df8b7e2139a0d2e32a24aa68e09fa
rob <rob@b7c.io>
2024-10-24 14:24:16 +1300

    fix manual wall item placement

--- src/Xabbo/Services/ManualWallItemPlacement.cs
+++ src/Xabbo/Services/ManualWallItemPlacement.cs
@@ -16,19 +16,50 @@ public sealed class ManualWallItemPlacement(
     private readonly IExtension _ext = extension;
     private readonly XabbotComponent _xabbot = xabbot;
 
-    public Task InitializeAsync(IRoom room) => Task.CompletedTask;
+    private IDisposable? _intercept;
+
+    private readonly Queue<Point> _points = [];
+    private readonly SemaphoreSlim _pointSemaphore = new(0);
+
+    public Task InitializeAsync(IRoom room)
+    {
+        _intercept?.Dispose();
+        _intercept = _ext.Intercept<WalkMsg>(HandleWalkMsg);
+        return Task.CompletedTask;
+    }
+
+    private void HandleWalkMsg(Intercept e, WalkMsg walk)
+    {
+        e.Block();
+
+        lock (_points)
+        {
+            _points.Enqueue(walk.Point);
+        }
+
+        _pointSemaphore.Release();
+    }
 
     public async Task<WallLocation?> FindLocationAsync(IRoom room, CancellationToken cancellationToken)
     {
+        int scale = room.FloorPlan.Scale;
+
         while (true)
         {
-            var wallTile = (await _ext.ReceiveAsync<WalkMsg>(timeout: -1, block: true, cancellationToken: cancellationToken)).Point;
-            if (!room.FloorPlan.IsWalkable(wallTile - (1, 0)))
-                return new WallLocation(wallTile - (1, 0), (0, 0), 'l');
-            else if (!room.FloorPlan.IsWalkable(wallTile - (0, 1)))
-                return new WallLocation();
-            else
-                _xabbot.ShowMessage("That is not a valid wall tile. Click a tile in front of a wall.");
+            await _pointSemaphore.WaitAsync(cancellationToken);
+
+            lock (_points)
+            {
+                if (!_points.TryDequeue(out Point wallTile))
+                    return null;
+
+                if (!room.FloorPlan.IsWalkable(wallTile - (0, 1)))
+                    return new WallLocation(wallTile - (0, 1), (scale/2, scale), 'r');
+                else if (!room.FloorPlan.IsWalkable(wallTile - (1, 0)))
+                    return new WallLocation(wallTile - (1, 0), (scale/2, scale), 'l');
+                else
+                    _xabbot.ShowMessage("That is not a valid wall tile. Click a tile in front of a wall.");
+            }
         }
     }
 
