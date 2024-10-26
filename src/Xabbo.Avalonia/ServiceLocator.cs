19d7ca95253f7754b8c0a3c7246c26b2b0d61cca
rob <rob@b7c.io>
2024-10-26 15:03:12 +1300

    clean up service locator

--- src/Xabbo.Avalonia/ServiceLocator.cs
+++ src/Xabbo.Avalonia/ServiceLocator.cs
@@ -1,11 +1,8 @@
-using System;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Splat;
-using Splat.Microsoft.Extensions.Logging;
 using Avalonia.Logging;
-using Avalonia.Controls.ApplicationLifetimes;
 using HanumanInstitute.MvvmDialogs;
 using HanumanInstitute.MvvmDialogs.Avalonia;
 
@@ -27,6 +24,7 @@ using Xabbo.Avalonia.Views;
 
 using IHostApplicationLifetime = Microsoft.Extensions.Hosting.IHostApplicationLifetime;
 using Splatr = Splat.SplatRegistrations;
+using System.ComponentModel;
 
 namespace Xabbo.Avalonia;
 
@@ -34,22 +32,36 @@ public static class ViewModelLocator
 {
     static ViewModelLocator()
     {
-        var container = Locator.CurrentMutable;
+        RegisterConfiguration();
+        RegisterLogging();
+        RegisterServices();
+        RegisterRepositories();
+        RegisterComponents();
+        RegisterControllers();
+        RegisterCommands();
+        RegisterViewModels();
+        RegisterViews();
 
-        // Settings
-        Splatr.RegisterLazySingleton<IAppPathProvider, AppPathService>();
-        Splatr.RegisterLazySingleton<IConfigProvider<AppConfig>, AppConfigProvider>();
+        Splatr.SetupIOC();
+    }
 
-        // Static configuration
+    static void RegisterConfiguration()
+    {
         var configRoot = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
             .Build();
         Splatr.RegisterConstant<IConfiguration>(configRoot);
+    }
 
-        // Logging
+    static void RegisterLogging()
+    {
+
         ILoggerFactory loggerFactory = LoggerFactory.Create(builder => {
             builder
-                .AddConfiguration(configRoot.GetSection("Logging"))
+                .AddConfiguration(Locator.Current.GetRequiredService<IConfiguration>().GetSection("Logging"))
+#if RELEASE
+                .SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.None)
+#endif
                 .AddSimpleConsole(console =>
                 {
                     console.SingleLine = true;
@@ -57,38 +69,124 @@ public static class ViewModelLocator
                     console.TimestampFormat = "[HH:mm:ss.fff] ";
                 });
         });
-        container.RegisterConstant(loggerFactory);
-        container.UseMicrosoftExtensionsLoggingWithWrappingFullLogger(loggerFactory);
+        Locator.CurrentMutable.RegisterConstant(loggerFactory);
 
         Logger.Sink = new AvaloniaLogSink(loggerFactory);
+    }
 
-        // Application services
+    static void RegisterServices()
+    {
+        Splatr.RegisterConstant(new GEarthOptions() with
         {
-            Lazy<IHostApplicationLifetime> lazy = new Lazy<IHostApplicationLifetime>(
-                () => new AvaloniaHostApplicationLifetime(
-                    Locator.Current.GetService<ILoggerFactory>()!,
-                    Locator.Current.GetService<IApplicationLifetime>()!
-                )
-            );
-            container.Register<Lazy<IHostApplicationLifetime>>(() => lazy);
-            container.Register<IHostApplicationLifetime>(() => {
-                return lazy.Value;
-            });
-        }
+            Name = "xabbo",
+            Description = "enhanced habbo",
+            Author = "b7"
+        });
+        Splatr.RegisterLazySingleton<GEarthExtension>();
 
+        Locator.CurrentMutable.Register<IExtension>(() => Locator.Current.GetService<GEarthExtension>());
+        Locator.CurrentMutable.Register<IRemoteExtension>(() => Locator.Current.GetService<GEarthExtension>());
+        Locator.CurrentMutable.Register<IInterceptor>(() => Locator.Current.GetService<GEarthExtension>());
+
+        Splatr.RegisterLazySingleton<IHostApplicationLifetime, AvaloniaHostApplicationLifetime>();
         Splatr.RegisterLazySingleton<IApplicationManager, XabboAppManager>();
         Splatr.RegisterLazySingleton<IUiContext, AvaloniaUiContext>();
         Splatr.RegisterLazySingleton<IClipboardService, ClipboardService>();
         Splatr.RegisterLazySingleton<ILauncherService, LauncherService>();
-        container.RegisterLazySingleton(() => (IDialogService)new DialogService(
+        Splatr.RegisterLazySingleton<GlobalExceptionHandler>();
+
+        // Configuration
+        Splatr.RegisterLazySingleton<IAppPathProvider, AppPathService>();
+        Splatr.RegisterLazySingleton<IConfigProvider<AppConfig>, AppConfigProvider>();
+
+        Locator.CurrentMutable.RegisterLazySingleton(() => (IDialogService)new DialogService(
             new DialogManager(
                 viewLocator: new ViewLocator(),
                 dialogFactory: new DialogFactory().AddFluent(FluentMessageBoxType.ContentDialog)),
             viewModelFactory: x => Locator.Current.GetService(x)));
 
-        Splatr.RegisterLazySingleton<GlobalExceptionHandler>();
+        // Xabbo services
+        Splatr.RegisterLazySingleton<IHabboApi, HabboApi>();
+        Splatr.RegisterLazySingleton<IFigureConverterService, FigureConverterService>();
+        Splatr.RegisterLazySingleton<IGameDataManager, GameDataManager>();
+        Splatr.RegisterLazySingleton<IGameStateService, GameStateService>();
+        Splatr.RegisterLazySingleton<IOperationManager, OperationManager>();
+        Splatr.RegisterLazySingleton<IPlacementFactory, PlacementFactory>();
 
-        // ViewModels
+        // Xabbo core components
+        Splatr.RegisterLazySingleton<ProfileManager>();
+        Splatr.RegisterLazySingleton<InventoryManager>();
+        Splatr.RegisterLazySingleton<RoomManager>();
+        Splatr.RegisterLazySingleton<TradeManager>();
+        Splatr.RegisterLazySingleton<FriendManager>();
+    }
+
+    static void RegisterRepositories()
+    {
+        Splatr.RegisterLazySingleton<IWardrobeRepository, JsonWardrobeRepository>();
+    }
+
+    static void RegisterComponents()
+    {
+        Splatr.RegisterLazySingleton<XabbotComponent>();
+        Splatr.RegisterLazySingleton<NotificationComponent>();
+        Splatr.RegisterLazySingleton<AvatarOverlayComponent>();
+
+        Splatr.RegisterLazySingleton<AntiHandItemComponent>();
+        Splatr.RegisterLazySingleton<AntiHcGiftNotificationComponent>();
+        Splatr.RegisterLazySingleton<AntiIdleComponent>();
+        Splatr.RegisterLazySingleton<AntiTradeComponent>();
+        Splatr.RegisterLazySingleton<AntiTurnComponent>();
+        Splatr.RegisterLazySingleton<AntiTypingComponent>();
+        Splatr.RegisterLazySingleton<AntiWalkComponent>();
+        Splatr.RegisterLazySingleton<ClickThroughComponent>();
+        Splatr.RegisterLazySingleton<RespectedComponent>();
+
+        Splatr.RegisterLazySingleton<ChatComponent>();
+        Splatr.RegisterLazySingleton<DoorbellComponent>();
+        Splatr.RegisterLazySingleton<FlattenRoomComponent>();
+        Splatr.RegisterLazySingleton<FurniActionsComponent>();
+        Splatr.RegisterLazySingleton<RoomEntryComponent>();
+        Splatr.RegisterLazySingleton<LightingComponent>();
+    }
+
+    static void RegisterControllers()
+    {
+        Splatr.RegisterLazySingleton<ControllerInitializer>();
+
+        Splatr.RegisterLazySingleton<ClickToController>();
+        Splatr.RegisterLazySingleton<RoomRightsController>();
+        Splatr.RegisterLazySingleton<RoomModerationController>();
+        Splatr.RegisterLazySingleton<RoomFurniController>();
+        Splatr.RegisterLazySingleton<FurniPlacementController>();
+        Splatr.RegisterLazySingleton<PrivacyController>();
+    }
+
+    static void RegisterCommands()
+    {
+        Locator.CurrentMutable.RegisterLazySingleton(() => Locator.Current.GetServices<CommandModule>());
+
+        Splatr.RegisterLazySingleton<CommandManager>();
+
+        #pragma warning disable SPLATDI006
+        Splatr.RegisterLazySingleton<CommandModule, AppCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, OperationCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, EffectCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, FindFriendCommand>();
+        Splatr.RegisterLazySingleton<CommandModule, VisibilityCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, FurniCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, InfoCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, ModerationCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, MoodCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, RoomCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, TurnCommand>();
+        Splatr.RegisterLazySingleton<CommandModule, UserProfileCommands>();
+        Splatr.RegisterLazySingleton<CommandModule, ClickThroughCommand>();
+        #pragma warning restore SPLATDI006
+    }
+
+    static void RegisterViewModels()
+    {
         Splatr.RegisterLazySingleton<MainViewModel>();
 
         Splatr.RegisterLazySingleton<GeneralPageViewModel>();
@@ -116,95 +214,11 @@ public static class ViewModelLocator
         Splatr.RegisterLazySingleton<ExternalVariablesViewModel>();
 
         Splatr.RegisterLazySingleton<OfferItemsViewModel>();
+    }
 
-        // Logic
-        Splatr.RegisterConstant(new GEarthOptions() with
-        {
-            Name = "xabbo",
-            Description = "enhanced habbo",
-            Author = "b7"
-        });
-        Splatr.RegisterLazySingleton<GEarthExtension>();
-        container.Register<IExtension>(() => Locator.Current.GetService<GEarthExtension>());
-        container.Register<IRemoteExtension>(() => Locator.Current.GetService<GEarthExtension>());
-        container.Register<IInterceptor>(() => Locator.Current.GetService<GEarthExtension>());
-
-        // Xabbo services
-        Splatr.RegisterLazySingleton<IGameStateService, GameStateService>();
-        Splatr.RegisterLazySingleton<IFigureConverterService, FigureConverterService>();
-        Splatr.RegisterLazySingleton<IWardrobeRepository, JsonWardrobeRepository>();
-        Splatr.RegisterLazySingleton<IHabboApi, HabboApi>();
-
-        Splatr.RegisterLazySingleton<FurniPlacementController>();
-        Splatr.RegisterLazySingleton<IPlacementFactory, PlacementFactory>();
-
-        // Xabbo core components
-        Splatr.RegisterLazySingleton<ProfileManager>();
-        Splatr.RegisterLazySingleton<InventoryManager>();
-        Splatr.RegisterLazySingleton<RoomManager>();
-        Splatr.RegisterLazySingleton<TradeManager>();
-        Splatr.RegisterLazySingleton<FriendManager>();
-
-        Splatr.RegisterLazySingleton<IGameDataManager, GameDataManager>();
-
-        // Xabbo components
-        Splatr.RegisterLazySingleton<IOperationManager, OperationManager>();
-
-        Splatr.RegisterLazySingleton<XabbotComponent>();
-        Splatr.RegisterLazySingleton<NotificationComponent>();
-        Splatr.RegisterLazySingleton<AvatarOverlayComponent>();
-
-        Splatr.RegisterLazySingleton<AntiHandItemComponent>();
-        Splatr.RegisterLazySingleton<AntiHcGiftNotificationComponent>();
-        Splatr.RegisterLazySingleton<AntiIdleComponent>();
-        Splatr.RegisterLazySingleton<AntiTradeComponent>();
-        Splatr.RegisterLazySingleton<AntiTurnComponent>();
-        Splatr.RegisterLazySingleton<AntiTypingComponent>();
-        Splatr.RegisterLazySingleton<AntiWalkComponent>();
-        Splatr.RegisterLazySingleton<ClickThroughComponent>();
-        Splatr.RegisterLazySingleton<RespectedComponent>();
-
-        Splatr.RegisterLazySingleton<ChatComponent>();
-        Splatr.RegisterLazySingleton<DoorbellComponent>();
-        Splatr.RegisterLazySingleton<FlattenRoomComponent>();
-        Splatr.RegisterLazySingleton<FurniActionsComponent>();
-        Splatr.RegisterLazySingleton<RoomEntryComponent>();
-        Splatr.RegisterLazySingleton<LightingComponent>();
-
-        // Controllers
-        Splatr.RegisterLazySingleton<ControllerInitializer>();
-
-        Splatr.RegisterLazySingleton<ClickToController>();
-        Splatr.RegisterLazySingleton<RoomRightsController>();
-        Splatr.RegisterLazySingleton<RoomModerationController>();
-        Splatr.RegisterLazySingleton<RoomFurniController>();
-        Splatr.RegisterLazySingleton<PrivacyController>();
-
-        // Xabbo commands
-        Splatr.RegisterLazySingleton<CommandManager>();
-
-        #pragma warning disable SPLATDI006
-        Splatr.RegisterLazySingleton<CommandModule, AppCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, OperationCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, EffectCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, FindFriendCommand>();
-        Splatr.RegisterLazySingleton<CommandModule, VisibilityCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, FurniCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, InfoCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, ModerationCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, MoodCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, RoomCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, TurnCommand>();
-        Splatr.RegisterLazySingleton<CommandModule, UserProfileCommands>();
-        Splatr.RegisterLazySingleton<CommandModule, ClickThroughCommand>();
-        #pragma warning restore SPLATDI006
-
-        container.RegisterLazySingleton(() => Locator.Current.GetServices<CommandModule>());
-
-        // Views
+    static void RegisterViews()
+    {
         Splatr.RegisterLazySingleton<MainWindow>();
-
-        Splatr.SetupIOC();
     }
 
     public static MainViewModel Main => Locator.Current.GetService<MainViewModel>()!;
